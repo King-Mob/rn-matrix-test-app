2f2b0f3e00a1cf09b424ef9600be0f3545c5b72b
Chaotic Capybara <chaoticcapybara@pm.me>
2021-03-02 22:06:26 -0500

    added basic ui (not completely working)

--- App.js
+++ App.js
@@ -6,23 +6,10 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 import 'node-libs-react-native/globals';
 import '@rn-matrix/core/shim.js';
@@ -31,80 +18,37 @@ import {polyfillGlobal} from 'react-native/Libraries/Utilities/PolyfillFunctions
 polyfillGlobal('URL', () => require('whatwg-url').URL);
 
 import { matrix } from '@rn-matrix/core';
+import {RoomList, MessageList} from '@rn-matrix/ui';
 
-const accessToken = '';
+import UserInfo from './UserInfo';
+
 const deviceId = '1234';
-matrix.createClient('https://matrix-client.matrix.org', accessToken, '', deviceId);
-matrix.start();
 
-console.log(Object.keys(matrix.getRooms$().getValue()[0]))
-console.log(matrix.getRooms$().getValue()[0]['name$'])
+const App = () => {
+  const [room, setRoom] = useState(null);
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Rooms</Text>
-              <Text style={styles.sectionDescription}>
-                
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
+  const handleRoomPress = (r) => {
+    setRoom(r);
+  };
+
+  useEffect(() => {
+    matrix.createClient(UserInfo.baseUrl, UserInfo.accessToken, UserInfo.userId, deviceId);
+    matrix.start(true);
+  }, []);
+
+  if (room) {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <MessageList room={room} enableComposer />
       </SafeAreaView>
-    </>
-  );
+    );
+  } else {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <RoomList onRowPress={handleRoomPress} />
+      </SafeAreaView>
+    );
+  }
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
